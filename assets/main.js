a4160cd3023b519bdcee41ead3a567f05adfa777
Zeha Irawan <jangkaringbumi@gmail.com>
2019-07-04 21:46:56 +0700

    set up fetch

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,16 @@
-const a = 1
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=London&APPID=9bf5614e989b618c0574d6238d81db95';
 
-a = 2
+// const key = '9bf5614e989b618c0574d6238d81db95';
 
-console.log(b)
+
+function basic() {
+  fetch(url, {
+    mode: 'cors',
+  })
+    .then(response => response.json())
+    .then((response) => {
+      console.log(response);
+    });
+}
+
+basic();
